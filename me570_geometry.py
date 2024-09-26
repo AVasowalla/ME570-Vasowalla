c96cf42522086d30df3f7029dcf71244102981fd
AVasowalla <108306331+AVasowalla@users.noreply.github.com>
2024-09-26 02:14:03 -0400

    hw 2 start

--- me570_geometry.py
+++ me570_geometry.py
@@ -3,6 +3,7 @@ Classes and functions for Polygons and Edges
 """
 
 import math
+import numbers
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -343,6 +344,174 @@ def angle(vertex0, vertex1, vertex2, angle_type="unsigned"):
         raise ValueError("Invalid argument angle_type")
 
     return edge_angle
+
+
+def gca_3d():
+    """
+    Get current Matplotlib axes, and if they do not support 3-D plotting,
+    add new axes that support it
+    """
+    fig = plt.gcf()
+    if len(fig.axes) == 0 or not hasattr(plt.gca(), "plot3D"):
+        axis = fig.add_subplot(111, projection="3d")
+    else:
+        axis = plt.gca()
+    return axis
+
+
+def numel(var):
+    """
+    Counts the number of entries in a numpy array, or returns 1 for fundamental numerical
+    types
+    """
+    if isinstance(var, numbers.Number):
+        size = int(1)
+    elif isinstance(var, np.ndarray):
+        size = var.size
+    else:
+        raise NotImplementedError(f"number of elements for type {type(var)}")
+    return size
+
+
+def rot2d(theta):
+    """
+    Create a 2-D rotation matrix from the angle  theta according to (1).
+    """
+    rot_theta = np.array(
+        [[math.cos(theta), -math.sin(theta)], [math.sin(theta), math.cos(theta)]]
+    )
+    return rot_theta
+
+
+def line_linspace(a_line, b_line, t_min, t_max, nb_points):
+    """
+    Generates a discrete number of  nb_points points along the curve
+    (t)=( a(1)t + b(1), a(2)t + b(2))  R^2 for t ranging from  tMin to  tMax.
+    """
+    t_sequence = np.linspace(t_min, t_max, nb_points)
+    theta_points = a_line * t_sequence + b_line
+    return theta_points
+
+
+class Grid:
+    """
+    A function to store the coordinates of points on a 2-D grid and evaluate arbitrary
+    functions on those points.
+    """
+
+    def __init__(self, xx_grid, yy_grid):
+        """
+        Stores the input arguments in attributes.
+        """
+        self.xx_grid = xx_grid
+        self.yy_grid = yy_grid
+
+    def eval(self, fun):
+        """
+        This function evaluates the function  fun (which should be a function)
+        on each point defined by the grid.
+        """
+
+        dim_domain = [numel(self.xx_grid), numel(self.yy_grid)]
+        dim_range = [numel(fun(np.array([[0], [0]])))]
+        fun_eval = np.nan * np.ones(dim_domain + dim_range)
+        for idx_x in range(0, dim_domain[0]):
+            for idx_y in range(0, dim_domain[1]):
+                x_eval = np.array([[self.xx_grid[idx_x]], [self.yy_grid[idx_y]]])
+                fun_eval[idx_x, idx_y, :] = np.reshape(
+                    fun(x_eval), [1, 1, dim_range[0]]
+                )
+
+        # If the last dimension is a singleton, remove it
+        if dim_range == [1]:
+            fun_eval = np.reshape(fun_eval, dim_domain)
+
+        return fun_eval
+
+    def mesh(self):
+        """
+        Shorhand for calling meshgrid on the points of the grid
+        """
+        return np.meshgrid(self.xx_grid, self.yy_grid)
+
+
+class Torus:
+    """
+    A class that holds functions to compute the embedding and display a torus and curves on it.
+    """
+
+    def phi(self, theta):
+        """
+        Implements equation (eq:chartTorus).
+        """
+        pass  # Substitute with your code
+        return x_torus
+
+    def plot(self):
+        """
+        For each one of the chart domains U_i from the previous question:
+        - Fill a  grid structure with fields  xx_grid and  yy_grid that define a grid of regular
+          point in U_i. Use nb_grid=33.
+        - Call the function Grid.eval with argument Torus.phi.
+        - Plots the surface described by the previous step using the the Matplotlib function
+        ax.plot_surface (where  ax represents the axes of the current figure) in a separate figure.
+        Plot a final additional figure showing all the charts at the same time.   To better show
+        the overlap between the charts, you can use different colors each one of them,
+        and making them slightly transparent.
+        """
+        pass  # Substitute with your code
+
+    def phi_push_curve(self, a_line, b_line):
+        """
+        This function evaluates the curve x(t)= phi_torus ( phi(t) )  R^3 at  nb_points=31 points
+        generated along the curve phi(t) using line_linspaceLine.linspace with  tMin=0 and  tMax=1,
+        and a, b as given in the input arguments.
+        """
+        pass  # Substitute with your code
+        return x_points
+
+    def plot_curves(self):
+        """
+        The function should iterate over the following four curves:
+        - 3/4*pi0
+        - 3/4*pi3/4*pi
+        - -3/4*pi3/4*pi
+        - 0 -3/4*pi  and  b=np.array([[-1],[-1]]).
+        The function should show an overlay containing:
+        - The output of Torus.plotCharts;
+        - The output of the functions torus_pushCurveTorus.pushCurve for each one of the curves.
+        """
+        a_lines = [
+            np.array([[3 / 4 * math.pi], [0]]),
+            np.array([[3 / 4 * math.pi], [3 / 4 * math.pi]]),
+            np.array([[-3 / 4 * math.pi], [3 / 4 * math.pi]]),
+            np.array([[0], [-3 / 4 * math.pi]]),
+        ]
+
+        b_line = np.array([[-1], [-1]])
+
+        axis = gca_3d()
+        for a_line in a_lines:
+            x_points = self.phi_push_curve(a_line, b_line)
+            axis.plot(x_points[0, :], x_points[1, :], x_points[2, :])
+
+    def phi_test(self):
+        """
+        Uses the function phi to plot two perpendicular rings
+        """
+        nb_points = 200
+        theta_ring = np.linspace(0, 15 / 8 * np.pi, nb_points)
+        theta_zeros = np.zeros((1, nb_points))
+        data = [
+            np.vstack((theta_ring, theta_zeros)),
+            np.vstack((theta_zeros, theta_ring)),
+        ]
+        axis = gca_3d()
+        for theta in data:
+            ring = np.zeros((3, nb_points))
+            for idx in range(nb_points):
+                ring[:, [idx]] = self.phi(theta[:, [idx]])
+            axis.plot(ring[0, :], ring[1, :], ring[2, :])
 
 
 if __name__ == "__main__":
