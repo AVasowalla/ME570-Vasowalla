08de6a0489baefb09529a988581df1cd47c4b058
BuildTools <108306331+AVasowalla@users.noreply.github.com>
2024-10-21 02:47:58 -0400

    fix

--- me570_robot.py
+++ me570_robot.py
@@ -170,8 +170,8 @@ class TwoLinkPotential:
         total = pot.Total(self.world, self.potential)
         w_p_eff = np.array(
             [
-                [5 * np.cos(np.sum(theta_eval) + 5 * np.cos(theta_eval[0, 0]))],
-                [5 * np.sin(np.sum(theta_eval) + 5 * np.sin(theta_eval[1, 0]))],
+                5 * np.cos(np.sum(theta_eval) + 5 * np.cos(theta_eval[0, 0])),
+                5 * np.sin(np.sum(theta_eval) + 5 * np.sin(theta_eval[1, 0])),
             ]
         )
         print(w_p_eff)
