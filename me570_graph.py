5fa65dec4621f3ca0d8cfcc82a4217fbcb6836ac
BuildTools <108306331+AVasowalla@users.noreply.github.com>
2024-11-20 10:26:25 -0500

    fix

--- me570_graph.py
+++ me570_graph.py
@@ -391,7 +391,7 @@ class SphereWorldGraph:
          - Store the resulting  graph object as an internal attribute.
         """
         self.sphereworld = pot.SphereWorld()
-        xx_grid = np.linspace(-5, 5, nb_cells)
+        xx_grid = np.linspace(-10, 10, nb_cells)
         self.grid = me570_geometry.Grid(xx_grid, xx_grid)
         potential = {
             "x_goal": self.sphereworld.x_goal[:, [0]],
@@ -531,6 +531,4 @@ def test_grid2graph():
     graph.plot()
 
 
-if __name__ == "__main__":
-    graph_test_data_plot()
-    plt.show()
+# if __name__ == "__main__":
